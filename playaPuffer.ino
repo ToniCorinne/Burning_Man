a150184ef55f5c203458cab08aecea071d15dd10
Toni Klopfenstein <toni.klopfenstein@sparkfun.com>
2018-08-16 21:52:47 -0600

    Adding playa puffer photocell controlled RGB strip

---
+++ playaPuffer.ino
@@ -0,0 +1,130 @@
+#include <FastLED.h>
+
+#define LED_DATA_PIN     7
+#define LED_CLOCK_PIN     9
+#define NUM_LEDS    12
+#define BRIGHTNESS  40
+#define LED_TYPE    LPD8806
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+#define UPDATES_PER_SECOND 100
+
+// This example shows several ways to set up and use 'palettes' of colors
+// with FastLED.
+//
+// These compact palettes provide an easy way to re-colorize your
+// animation on the fly, quickly, easily, and with low overhead.
+//
+// USING palettes is MUCH simpler in practice than in theory, so first just
+// run this sketch, and watch the pretty lights as you then read through
+// the code.  Although this sketch has eight (or more) different color schemes,
+// the entire sketch compiles down to about 6.5K on AVR.
+//
+// FastLED provides a few pre-configured color palettes, and makes it
+// extremely easy to make up your own color schemes with palettes.
+//
+// Some notes on the more abstract 'theory and practice' of
+// FastLED compact palettes are at the bottom of this file.
+
+
+/********* Initialization *************/
+
+CRGBPalette16 currentPalette;
+TBlendType    currentBlending;
+
+const int LIGHT_PIN = A0; // Pin connected to voltage divider output
+const float VCC = 4.98; // Measured voltage of Ardunio 5V line
+const float R_DIV = 4660.0; // Measured resistance of 3.3k resistor
+const float DARK_THRESHOLD = 10000.0;
+
+/********* Setup Loop *************/
+void setup() {
+    delay( 3000 ); // power-up safety delay
+    FastLED.addLeds<LED_TYPE, LED_DATA_PIN, LED_CLOCK_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.setBrightness(  BRIGHTNESS );
+    
+    currentPalette = RainbowColors_p;
+    currentBlending = LINEARBLEND;
+
+      pinMode(LIGHT_PIN, INPUT);
+
+}
+
+/********* Main Loop *************/
+void loop()
+{
+   int lightADC = analogRead(LIGHT_PIN);
+  if (lightADC > 0)
+  {
+    // Use the ADC reading to calculate voltage and resistance
+    float lightV = lightADC * VCC / 1023.0;
+    float lightR = R_DIV * (VCC / lightV - 1.0);
+    Serial.println("Voltage: " + String(lightV) + " V");
+    Serial.println("Resistance: " + String(lightR) + " ohms");
+
+    // If resistance of photocell is greater than the dark
+    // threshold setting, turn the LED on.
+    if (lightR >= DARK_THRESHOLD){
+      ChangePalettePeriodically();
+      
+      static uint8_t startIndex = 0;
+      startIndex = startIndex + 1; /* motion speed */
+      
+      FillLEDsFromPaletteColors( startIndex);
+      
+      FastLED.show();
+      FastLED.delay(1000 / UPDATES_PER_SECOND);
+    }
+    else{
+     fill_solid( currentPalette, 16, CRGB::Black);
+    }
+    
+    }
+  }
+
+/********* Fill From Palette *************/
+void FillLEDsFromPaletteColors( uint8_t colorIndex)
+{
+    uint8_t brightness = 255;
+    
+    for( int i = 0; i < NUM_LEDS; i++) {
+        leds[i] = ColorFromPalette( currentPalette, colorIndex, brightness, currentBlending);
+        colorIndex += 3;
+    }
+}
+
+
+// There are several different palettes of colors demonstrated here.
+//
+// FastLED provides several 'preset' palettes: RainbowColors_p, RainbowStripeColors_p,
+// OceanColors_p, CloudColors_p, LavaColors_p, ForestColors_p, and PartyColors_p.
+//
+// Additionally, you can manually define your own color palettes, or you can write
+// code that creates color palettes on the fly.  All are shown here.
+
+/********* Change Palette *************/
+void ChangePalettePeriodically()
+{
+    uint8_t secondHand = (millis() / 1000) % 60;
+    static uint8_t lastSecond = 99;
+    
+    if( lastSecond != secondHand) {
+        lastSecond = secondHand;
+        if( secondHand ==  0)  { currentPalette = RainbowColors_p;         currentBlending = LINEARBLEND; }
+        if( secondHand == 10)  { currentPalette = ForestColors_p;          currentBlending = LINEARBLEND; }
+        if( secondHand == 15)  { currentPalette = RainbowStripeColors_p;   currentBlending = LINEARBLEND; }
+        if( secondHand == 25)  { SetupTotallyRandomPalette();              currentBlending = LINEARBLEND; }
+        if( secondHand == 40)  { currentPalette = CloudColors_p;           currentBlending = LINEARBLEND; }
+        if( secondHand == 45)  { currentPalette = OceanColors_p;           currentBlending = LINEARBLEND; }
+    }
+}
+
+/********* Randomize Palette *************/
+void SetupTotallyRandomPalette()
+{
+    for( int i = 0; i < 16; i++) {
+        currentPalette[i] = CHSV( random8(), 255, random8());
+    }
+}
+
